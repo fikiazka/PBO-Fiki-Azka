8f0763fc1a147d0773c630eeda3bbb343a22ff86
fiki azka <fikiiazka@gmail.com>
2026-09-13 19:17:59 +0700

    modif max kecepatan 100

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -22,8 +22,8 @@ public class Motor {
     }
 
     public void setKecepatan(int kecepatan) {
-        if (!this.statusMesin && kecepatan > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 saat mesin off");
+        if (kecepatan > 100) {
+            System.out.println("Kecepatan maksimal adalah 100");
         } else {
             this.kecepatan = kecepatan;
         }
--- Praktikum03/MotorDemo.java
+++ Praktikum03/MotorDemo.java
@@ -15,7 +15,7 @@ public class MotorDemo {
 
         Motor motor3 = new Motor();
         motor3.setPlatNomor("D 8343 CV");
-        motor3.setKecepatan(60);
+        motor3.setKecepatan(120);
         motor3.displayInfo();
     }
 }
